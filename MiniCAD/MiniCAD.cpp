a252698e33027b140ff97eacd7edac98888ae536
Lumin <sweet_0424@qq.com>
2018-11-29 22:15:30 +0800

    .

--- MiniCAD/MiniCAD.cpp
+++ MiniCAD/MiniCAD.cpp
@@ -4,10 +4,11 @@ MiniCAD::MiniCAD(QWidget *parent)
 	: QMainWindow(parent)
 {
 	ui.setupUi(this);
-	//connect(m_ui.checkBoxShowLines, SIGNAL(toggled(bool)), m_ui.openGLWidget, SLOT(onDrawLinesCheckBoxToggled(bool)));
-	connect(ui.checkBox_lineShow, SIGNAL(toggled(bool)), ui.showWidget, SLOT(onDrawLinesCheckBoxSlot(bool)));
-	connect(ui.checkBox_volumeShow, SIGNAL(toggled(bool)), ui.showWidget, SLOT(onDrawFacesCheckBoxSlot(bool))),
 
-	//ui.checkBox_volumeShow->setCheckState(Qt::Checked);
+	connect(ui.checkBox_lineShow, SIGNAL(toggled(bool)), ui.showWidget, SLOT(onDrawLinesCheckBoxSlot(bool)));
+	connect(ui.checkBox_volumeShow, SIGNAL(toggled(bool)), ui.showWidget, SLOT(onDrawFacesCheckBoxSlot(bool)));
+	
+
 	ui.checkBox_lineShow->setCheckState(Qt::Checked);
+	//ui.checkBox_volumeShow->setCheckState(Qt::Checked);
 }
